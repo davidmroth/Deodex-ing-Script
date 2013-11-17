93c8e02692012ff5c852cc6b2443e3843af0e44a
Neel Kowdley <nkowdley@gmail.com>
2013-11-17 03:15:09 -0500

    Update deodex.sh
    I didn't know the best way of undoing my commits, so I reverted to what is originally on mistadman's github

--- deodex.sh
+++ deodex.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
 ERROR=0
-smalibaksmali_dir=/home/neel/smali
+smalibaksmali_dir=/home/mistadman/bin/android_dev_tools
 
 clear; for x in `find -iname "*.odex"|sort`; do 
     odexFile=${x/\.\//}
-    [ -e ${x/odex/jar} ] && JarFile=${odexFile/odex/jar} || JarFile=${odexFile/odex/apk}
+    [ -e ${x/odex/jar} ] && JarFile=${odexFile/odex/jar} || Jarfile=${odexFile/odex/apk}
 
     echo "Uncompiling $odexFile"
-    java -Xmx1024m -jar $smalibaksmali_dir/baksmali.jar -x $odexFile -d $1 -o /tmp/$odexFile.out 
+    java -Xmx512m -jar $smalibaksmali_dir/baksmali.jar -x $odexFile -o /tmp/$odexFile.out 
 
     if [ -e /tmp/$odexFile.out ]; then
-        java -Xmx1024m -jar $smalibaksmali_dir/smali.jar /tmp/$odexFile.out -o /tmp/$odexFile-classes.dex
+        java -Xmx512m -jar $smalibaksmali_dir/smali.jar /tmp/$odexFile.out -o /tmp/$odexFile-classes.dex
         ERROR=1
     fi
 
     if [ -e /tmp/$odexFile-classes.dex ]; then
         echo "Adding classes.dex to $JarFile"
         mv /tmp/$odexFile-classes.dex /tmp/classes.dex
-        zip $JarFile /tmp/classes.dex
+        zip -q $JarFile /tmp/classes.dex
         rm -rf /tmp/$odexFile.out /tmp/$odexFile-classes.dex /tmp/classes.dex
     else
         rm -rf /tmp/$odexFile.out /tmp/$odexFile-classes.dex /tmp/classes.dex
